f7875f00f26cb23174ca1da346fd0776720e6b9d
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2023-12-26 22:16:45 +0100

    fixed joystick for mobile use

--- src/views/Model.vue
+++ src/views/Model.vue
@@ -1,15 +1,16 @@
 <template>
   <canvas ref="canvas_ref" />
-  <!-- <Joystick /> -->
+  <LoadingSpinner v-if="is_loading" />
+  
   <div class="keys">
     <div />
-    <div class="key" @pointerup="onKeyup" @pointerdown="up">↑</div>
+    <div class="key" :class="{ 'active' : pointer_down == 'up' }" @pointerup="onKeyup" @pointerdown="move('up')">↑</div>
     <div />
-    <div class="key" @pointerup="onKeyup" @pointerdown="left">←</div>
+    <div class="key" :class="{ 'active' : pointer_down == 'left' }" @pointerup="onKeyup" @pointerdown="move('left')">←</div>
     <div />
-    <div class="key" @pointerup="onKeyup" @pointerdown="right">→</div>
+    <div class="key" :class="{ 'active' : pointer_down == 'right' }" @pointerup="onKeyup" @pointerdown="move('right')">→</div>
     <div />
-    <div class="key" @pointerup="onKeyup" @pointerdown="down">↓</div>
+    <div class="key" :class="{ 'active' : pointer_down == 'down' }" @pointerup="onKeyup" @pointerdown="move('down')">↓</div>
     <div />
   </div>
 </template>
@@ -23,7 +24,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import GUI from "lil-gui";
-// import Joystick from "../components/Joystick.vue";
+import LoadingSpinner from "../components/LoadingSpinner.vue";
 
 // ==============
 // Variables
@@ -35,10 +36,12 @@ const sizes = reactive({
   width: window.innerWidth,
   height: window.innerHeight,
 });
+const is_loading = ref( true );
+const pointer_down = ref( false );
 const aspect_ratio = computed(() => sizes.width / sizes.height);
 const spider_model_url = "/models/spider.glb";
 const assetLoader = new GLTFLoader();
-const gui = new GUI();
+// const gui = new GUI();
 
 let mixer = undefined;
 let animation_frame = undefined;
@@ -46,11 +49,11 @@ let model = undefined;
 let spider_default_animation = undefined;
 let spider_walk_animation = undefined;
 let timeout = undefined;
+let renderer = null;
+let controls;
 
 const scene = new THREE.Scene();
-let renderer = null;
 let clock = new THREE.Clock();
-let controls;
 const camera = new THREE.PerspectiveCamera(FOV, aspect_ratio.value);
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
 scene.add(ambientLight);
@@ -74,9 +77,10 @@ scene.add(plane);
 // Functions
 // ==============
 function onLoadAsset(ev) {
+  is_loading.value = false;
   model = ev.scene;
   model.position.y = -0.5;
-  model.position.z = 1;
+  model.position.z = -0.5;
   mixer = new THREE.AnimationMixer(model);
   spider_walk_animation = mixer.clipAction(
     ev.animations.find((a) => a.name == "spider.walk")
@@ -102,6 +106,7 @@ function gameLoop() {
 }
 
 function onResize() {
+  console.log( camera.position )
   // update sizes
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
@@ -115,97 +120,57 @@ function onResize() {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 }
 
-function onDblclick() {
-  const fullscreenElement =
-    document.fullscreenElement || document.webkitFullscreenElement;
-
-  if (fullscreenElement) {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    }
-    return;
-  }
-
-  if (canvas_ref.value.requestFullscreen) {
-    canvas_ref.value.requestFullscreen();
-  } else if (canvas_ref.value.webkitRequestFullscreen) {
-    canvas_ref.value.webkitRequestFullscreen();
-  }
-}
-
 function onKeyup() {
   clearTimeout( timeout );
   timeout = undefined;
   spider_walk_animation.stop();
   spider_default_animation.play();
+  pointer_down.value = false;
 }
 
-function up() {
+function move(direction) {
   spider_walk_animation.play();
   spider_default_animation.stop();
-  model.position.z += SPEED;
-  timeout = setTimeout( up, 100 );
-}
-function down() {
-  spider_walk_animation.play();
-  spider_default_animation.stop();
-  model.position.z -= SPEED;
-  timeout = setTimeout( down, 100 );
+  pointer_down.value = direction;
 
-}
-
-function left() {
-  spider_walk_animation.play();
-  spider_default_animation.stop();
-  model.position.x += SPEED;
-  timeout = setTimeout( left, 100 );
-}
-
-function right() {
-  spider_walk_animation.play();
-  spider_default_animation.stop();
-  model.position.x -= SPEED;
-  timeout = setTimeout( right, 100 );
-}
-
-function onKeydown(e) {
-  if (!["a", "d", "w", "s"].includes(e.key)) {
-    return;
-  }
-  spider_walk_animation.play();
-  spider_default_animation.stop();
-  switch (e.key) {
-    case "a":
-      model.position.x += SPEED;
-      break;
-    case "d":
-      model.position.x -= SPEED;
-      break;
-    case "w":
+  switch (direction) {
+    case 'up':
       model.position.z += SPEED;
       break;
-    case "s":
+    case 'down':
       model.position.z -= SPEED;
       break;
+    case 'left':
+      model.position.x += SPEED;
+      break;
+    case 'right':
+      model.position.x -= SPEED;
+      break;
+    default:
+      break;
   }
+
+  timeout = setTimeout(() => move(direction), 100);
 }
+
+
 
 // ==============
 // Life cycle
 // ==============
 onMounted(() => {
   window.addEventListener("resize", onResize);
-  window.addEventListener("dblclick", onDblclick);
-  window.addEventListener("keydown", onKeydown);
   window.addEventListener("keyup", onKeyup);
   assetLoader.load(spider_model_url, onLoadAsset);
   renderer = new THREE.WebGLRenderer({ canvas: canvas_ref.value });
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   renderer.shadowMap.enabled = true;
-  camera.position.z = 3;
+
+  camera.position.z = -2;
+  camera.position.y = 2;
+  camera.position.x = 0;
+  
   controls = new OrbitControls(camera, canvas_ref.value);
   controls.enableDamping = true;
   gameLoop();
@@ -214,10 +179,8 @@ onMounted(() => {
 onUnmounted(() => {
   cancelAnimationFrame(animation_frame);
   window.removeEventListener("resize", onResize);
-  window.removeEventListener("dblclick", onDblclick);
-  window.removeEventListener("keydown", onKeydown);
   window.removeEventListener("keyup", onKeyup);
-  gui.hide();
+  // gui.hide();
   clearTimeout( timeout );
   timeout = undefined;
 });
@@ -232,6 +195,7 @@ onUnmounted(() => {
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr;
   -webkit-tap-highlight-color: transparent;
+  user-select: none;
   .key {
     display: grid;
     place-content: center;
@@ -239,7 +203,14 @@ onUnmounted(() => {
     height: 46px;
     border-radius: 50%;
     border: 1px solid #eee;
+    transition-duration: 400ms;
     color: #eee;
+    &.active {
+      background-color: #eee;
+      color: #222;
+      transition-duration: 400ms;
+      transform: scale(0.9);
+    }
   }
 }
 </style>
